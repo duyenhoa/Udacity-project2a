7cde76c7ba21252bfc51c8ec762b2ace8e160b7e
root <admin@example.com>
2022-06-13 23:22:19 +0900

    udacity project

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://hoaproject2.azurewebsites.net:$PORT/predict 
+     -X POST https://yenhoaudacity.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
