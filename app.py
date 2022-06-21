5dfc0470321aea9151b8cbf332009e0fefeaa529
duyenhoa <48123914+duyenhoa@users.noreply.github.com>
2022-06-21 23:56:25 +0900

    update

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import logging
 
 import pandas as pd
 #from sklearn.externals import joblib
-from jinja2.utils import markupsafe
-markupsafe.Markup()
-Markup('')
+#from jinja2.utils import markupsafe
+#markupsafe.Markup()
+#Markup('')
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
